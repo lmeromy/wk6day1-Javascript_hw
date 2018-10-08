ae2d7212df670487531c729ebfd013bd55019d3b
Leah Meromy <lmeromy@gmail.com>
2018-10-08 16:38:06 +0100

    Homework complete - Javascript Week6Day1

---
+++ hw_day1.js
@@ -0,0 +1,228 @@
+
+
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why. Copy the following episodes into a JavaScript file and add comments under each one detailing the reason for your predicted output.
+//
+// MVP
+// ------------------------------
+// Episode 1
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// OUTCOME: "The murderer is Miss Scarlett"
+
+// -----------------------------------
+// Episode 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+// OUTCOME: "The murderer is Professor Plum"
+// Why? murderer is a const, so cannot be re-assigned
+// Why is it also returning "undefined?"
+// -----------------------------------
+
+// Episode 3
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// First verdict: "The murderer is Mrs. Peacock" b/c it is calling the declareMurderer function, and she is defined as the murderer within
+// Second verdict: "The murderer is Professor Plum" b/c lexical scope...Prof Plum as murderer is global
+
+// -----------------------------------
+// Episode 4
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// OUTCOME:
+// 1. The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// b/c the declareAllSuspects function overwrites suspect3 to be Col. Must
+// 2. will return "Suspect three is Mrs. Peacock."
+// 1st time suspect3 was declared was with let, and declareAllSuspects was not called, so it was never overwritten!
+
+// -----------------------------------
+// Episode 5
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+// OUTCOME: The weapon is the Revolver.
+// why? the change weapon function overwrote the weapon in scenario
+// -----------------------------------
+
+// Episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// OUTCOME: The murderer is Mrs. White.
+// Using the plotTwist function within the changeMurderer function, the murderer has twice been overwritten to be Mrs. White
+
+// -----------------------------------
+
+// Episode 7
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// OUTCOME: The murderer is Mr. Green.
+// Without const or let or var, murderer = Mr. Green is now global
+
+// -----------------------------------
+
+// Episode 8
+
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// OUTCOME: The weapon is Candle Stick.
+// Why: unexpectedOutcome and plotTwist were both triggered so the weapon becomes the new one in unexpectedOutcome
+
+// -----------------------------------
+
+// Episode 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// OUTCOME: The murderer is PRof Plum.
+// Why: The conditional statement was never actually called! so the murderer remains Prof Plum.
+
+// -----------------------------------
+// Extensions
+// Make up your own episode!
